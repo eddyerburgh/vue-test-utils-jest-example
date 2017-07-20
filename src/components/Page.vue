7f1b6b56b6c04c6f038c48367b6c7ed481f5d476
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-07-20 08:09:01 +0100

    feat(list): add list component

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <message-toggle />
+        <list :items="['list item 1', 'listen item 2']" />
     </div>
 </template>
 
 <script>
     import MessageToggle from './MessageToggle'
+    import List from './List'
 
     export default {
       name: 'page',
       components: {
-        MessageToggle
+        MessageToggle,
+        List
       }
     }
 </script>
