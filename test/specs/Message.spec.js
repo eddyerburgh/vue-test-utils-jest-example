bbe0ab9e99a5c2589144657c88a7f2210f0e1e1c
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-07-20 08:01:51 +0100

    refactor: add spaces to shallow import

--- test/specs/Message.spec.js
+++ test/specs/Message.spec.js
@@ -1,4 +1,4 @@
-import {shallow} from 'vue-test-utils'
+import { shallow } from 'vue-test-utils'
 import Message from 'src/components/Message'
 
 describe('Message', () => {
--- test/specs/MessageToggle.spec.js
+++ test/specs/MessageToggle.spec.js
@@ -1,4 +1,4 @@
-import {shallow} from 'vue-test-utils'
+import { shallow } from 'vue-test-utils'
 import MessageToggle from 'src/components/MessageToggle.vue'
 import Message from 'src/components/Message'
 
