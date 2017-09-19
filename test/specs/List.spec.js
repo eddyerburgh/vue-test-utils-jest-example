d78617dab52ae1bad4c5794de44fe23e10fef28e
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-19 08:54:39 +0100

    feat(snapshots): add snapshot tests

--- test/specs/List.spec.js
+++ test/specs/List.spec.js
@@ -1,5 +1,10 @@
-import { shallow } from 'vue-test-utils'
+import {
+  shallow
+} from 'vue-test-utils'
 import List from '@/components/List.vue'
+import {
+  createRenderer
+} from 'vue-server-renderer'
 
 describe('List.vue', () => {
   it('renders li for each item in props.items', () => {
@@ -8,5 +13,17 @@ describe('List.vue', () => {
       propsData: { items }
     })
     expect(wrapper.findAll('li')).toHaveLength(items.length)
+  })
+
+  it('matches snapshot', () => {
+    const items = ['item 1', 'item 2']
+    const renderer = createRenderer()
+    const wrapper = shallow(List, {
+      propsData: { items }
+    })
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      if (err) throw new Error(err)
+      expect(str).toMatchSnapshot()
+    })
   })
 })
